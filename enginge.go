25bec7b71611fdd623202e6c0f6b91a83a48f890
Sagleft <sagleft@inbox.ru>
2022-08-02 20:54:20 +0300

    join channels

--- enginge.go
+++ enginge.go
@@ -12,7 +12,15 @@ type ChatBot struct {
 
 type ChatBotData struct {
 	Client *utopiago.UtopiaClient `json:"client"`
-	Chats  []string               `json:"chats"` // channel ids
+	Chats  []Chat                 `json:"chats"` // channel ids
+}
+
+type Chat struct {
+	// required
+	ID string `json:"id"`
+
+	// optional
+	Password string `json:"password"`
 }
 
 // NewChatBot - create new chatbot and connect to Utopia.
@@ -25,6 +33,18 @@ func NewChatBot(data ChatBotData) (*ChatBot, error) {
 	// check connection
 	if !cb.data.Client.CheckClientConnection() {
 		return nil, errors.New("failed to connect to Utopia Client")
+	}
+
+	// join to channels (chats)
+	for _, chat := range cb.data.Chats {
+		isJoined, err := cb.data.Client.JoinChannel(chat.ID, chat.Password)
+		if err != nil {
+			return cb, err
+		}
+
+		if !isJoined {
+			return cb, errors.New("failed to join in " + chat.ID)
+		}
 	}
 
 	return cb, nil

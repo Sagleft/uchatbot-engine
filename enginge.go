104b19b2ca199f413a35b42419183833380d06ca
Sagleft <sagleft@inbox.ru>
2022-10-15 01:19:27 +0300

    use reconnect callback in default oerr

--- enginge.go
+++ enginge.go
@@ -177,6 +177,14 @@ func (c *ChatBot) onError(err error) {
 		return
 	}
 
+	if strings.Contains(err.Error(), "reset by peer") {
+		if c.data.UseReconnectCallback {
+			c.data.Callbacks.ReconnectCallback()
+		} else {
+			log.Println("ERROR: " + err.Error())
+		}
+	}
+
 	if c.data.UseErrorCallback {
 		c.data.ErrorCallback(err)
 	} else {
